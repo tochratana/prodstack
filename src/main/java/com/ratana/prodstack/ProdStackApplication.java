a8bb6a135080ec57e14a725c81f18f8194a79426
RML-Cambo <ltomusic69@gmail.com>
2025-12-20 17:58:15 +0700

    add: init

---
+++ src/main/java/com/ratana/prodstack/ProdStackApplication.java
@@ -0,0 +1,13 @@
+package com.ratana.prodstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdStackApplication.class, args);
+    }
+
+}
